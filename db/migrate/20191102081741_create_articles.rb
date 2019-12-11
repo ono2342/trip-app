054a523e04ccad678f9776bdc95acc6bf107a50a
ono2342 <56573437+ono2342@users.noreply.github.com>
2019-12-11 19:08:11 +0900

    commit

--- db/migrate/20191102081741_create_articles.rb
+++ db/migrate/20191102081741_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration[5.0]
     create_table :articles do |t|
       t.string :title
       t.text :text
-      t.references :country, foreign_key: true
+      t.integer :country_id
       t.references :user, foreign_key: true
       t.timestamps null:true
     end
--- db/migrate/20191106064941_add_images_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddImagesToArticles < ActiveRecord::Migration[5.0]
-  def change
-    add_column :articles, :image, :string
-  end
-end
