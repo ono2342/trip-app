965ddacdb7626ae96b75ed6f25117c0f3edeab34
ono2342 <56573437+ono2342@users.noreply.github.com>
2020-01-14 16:24:38 +0900

    change design

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,5 +1,8 @@
 class TopsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
     @countries = Country.all
+    @articles = Article.all
   end
 end
